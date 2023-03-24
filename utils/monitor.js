05ebe257e2434416c0ec11ed8635544f385c66f7
Mahmoud Samir <menatosamir55@gmail.com>
2023-03-24 17:52:43 +0200

    send email on fails

--- utils/monitor.js
+++ utils/monitor.js
@@ -1,6 +1,8 @@
 const axios = require("axios");
+const sendEmail = require("./sendMail");
 const Check = require("../models/checkModel");
 const Report = require("../models/reportModel");
+const User = require('../models/userModel')
 
 const updateReportStatus = async (doc) => {
   let sum = 0;
@@ -33,7 +35,7 @@ const updateReportStatus = async (doc) => {
   }
 };
 
-const checkUrl = async (check) => {
+const checkUrl = async (check, threshold = 1) => {
   // Find report or create if not exist
   let report = await Report.findOne({ request: check._id });
   if (!report) {
@@ -41,7 +43,7 @@ const checkUrl = async (check) => {
   }
   // Setup Needed Vars
   let startTime = new Date().getTime(); // Help to count the request Response time
-  const requestTimeOut = check.timeout * 1000 || 5000; // Set the timeout in MS it 
+  const requestTimeOut = check.timeout * 1000 || 5000; // Set the timeout in MS it
   //Create  Headers Object
   let headers = {};
   if (check.httpHeaders) {
@@ -52,22 +54,22 @@ const checkUrl = async (check) => {
     });
   }
   //Create Authentication Object
-  let auth = {}
-  if(check.authentication){
-    auth.username = check.authentication.username
-    auth.password = check.authentication.password
+  let auth = {};
+  if (check.authentication) {
+    auth.username = check.authentication.username;
+    auth.password = check.authentication.password;
   }
   // Create the request URL
-  let url = check.url
-  if(check.path){
-    url = url + check.path
+  let url = check.url;
+  if (check.path) {
+    url = url + check.path;
   }
   //Send Request
   axios
     .get(url, { timeout: requestTimeOut, auth, headers })
     .then(async (response) => {
-      const endTime = new Date().getTime(); // End the request response time 
-      const responseTime = endTime - startTime; 
+      const endTime = new Date().getTime(); // End the request response time
+      const responseTime = endTime - startTime;
       // Push the success polling request into the history
       report = await Report.findByIdAndUpdate(
         report._id,
@@ -87,6 +89,9 @@ const checkUrl = async (check) => {
       await updateReportStatus(report);
     })
     .catch(async (error) => {
+      if (threshold < check.threshold) {
+        checkUrl(check, threshold + 1);
+      }
       const endTime = new Date().getTime();
       const responseTime = endTime - startTime;
       // Push the Failed polling request into the history
@@ -107,6 +112,12 @@ const checkUrl = async (check) => {
       );
       // Do the Requests response caculations
       await updateReportStatus(report);
+      const user = await User.findById(check.owner)
+      await sendEmail({
+        email: user.email,
+        subject: "URL Down",
+        message: `Your URL ${url} is Down right now`,
+      });
     });
 };
 
